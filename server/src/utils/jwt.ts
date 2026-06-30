eb06dd6ab151dfff0bd54fbc486785b2a60aae9f
Vignesh-737 <5737vignesh@gmail.com>
2026-06-30 22:30:31 +0530

    Auth

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken=(userId: string)=>{
+    try {
+        const token =jwt.sign({
+    userId
+  },
+  process.env.JWT_SECRET!,
+  {
+    expiresIn: "7d"
+  })
+  return token
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+}
+
+export const verifyToken=(token:string)=>{
+    try {
+       const decoded = jwt.verify(token,process.env.JWT_SECRET!)
+        return decoded
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+
+}
