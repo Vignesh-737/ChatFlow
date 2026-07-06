0a7e31a202b9a412d7b26358e6e80029fad75a7f
Vignesh-737 <5737vignesh@gmail.com>
2026-07-06 21:36:59 +0530

    feat(messages): implement secure message retrieval

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -60,3 +60,66 @@ export const sendMessage = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+type MessageParams = {
+  conversationId: string;
+};
+
+export const getMessages = async (req: Request<MessageParams>,res: Response) => {
+  try {
+    const myUserId = req.user.userId;
+    const { conversationId } = req.params;
+
+    // Validation
+    if (!conversationId) {
+      return res.status(400).json({
+        message: "Conversation ID is required",
+      });
+    }
+
+    // Verify conversation exists AND user belongs to it
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        id: conversationId,
+        members: {
+          some: {
+            userId: myUserId,
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({
+        message: "Conversation not found or access denied",
+      });
+    }
+
+    // Fetch messages
+    const messages = await prisma.message.findMany({
+      where: {
+        conversationId,
+      },
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return res.status(200).json(messages);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
--- server/src/routes/message.routes.ts
+++ server/src/routes/message.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage,getMessages } from "../controllers/message.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", authMiddleware, sendMessage);
+router.get("/:conversationId", authMiddleware, getMessages);
+
 
 export default router;
